3faf495f3ccc6bc2e825fa4580b01d0bac286b95
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2025-05-22 02:10:44 -0300

    chore(core): create footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState, type FormEvent } from 'react'
 import Image from 'next/image'
 
 import { FreightForm } from '@/components/freight-form'
+import { Footer } from '@/components/footer'
 
 import { api } from '@/services/api'
 
@@ -109,82 +110,85 @@ export default function ProductPage() {
   }
 
   return (
-    <main className="max-w-5xl mx-auto h-screen place-items-center p-4 grid grid-cols-1 md:grid-cols-2 gap-6">
-      <section>
-        <Image
-          src={mainImage}
-          alt="Produto principal"
-          width={250}
-          height={250}
-          className="rounded-lg object-cover size-80"
-        />
-        <div className="flex gap-2 mt-4">
-          {productImages.map((productImage, index) => (
-            <Image
-              key={index}
-              src={productImage}
-              alt={`Miniatura ${index}`}
-              width={50}
-              height={50}
-              className={`cursor-pointer border ${
-                mainImage === productImage
-                  ? 'border-blue-500'
-                  : 'border-gray-300'
-              }`}
-              onClick={() => setMainImage(productImage)}
-            />
-          ))}
-        </div>
-      </section>
-      <section className="space-y-6">
-        <div>
-          <h1 className="text-3xl font-semibold">Camiseta Estilosa</h1>
-          <p className="text-xl mt-1 text-gray-600 font-bold">R$ 79,90</p>
-        </div>
-        <div className="space-y-2">
-          <h2 className="font-medium">Tamanho:</h2>
-          <div className="flex gap-2">
-            {variants.size.map((variant) => (
-              <button
-                key={variant}
-                onClick={() => setSelectedSize(variant)}
-                className={`px-4 py-2 border rounded ${
-                  selectedSize === variant
-                    ? 'bg-blue-600 text-white'
-                    : 'bg-white border-gray-300'
+    <>
+      <main className="max-w-5xl mx-auto h-screen place-items-center p-4 grid grid-cols-1 md:grid-cols-2 gap-6">
+        <section>
+          <Image
+            src={mainImage}
+            alt="Produto principal"
+            width={250}
+            height={250}
+            className="rounded-lg object-cover size-80"
+          />
+          <div className="flex gap-2 mt-4">
+            {productImages.map((productImage, index) => (
+              <Image
+                key={index}
+                src={productImage}
+                alt={`Miniatura ${index}`}
+                width={50}
+                height={50}
+                className={`cursor-pointer border ${
+                  mainImage === productImage
+                    ? 'border-blue-500'
+                    : 'border-gray-300'
                 }`}
-              >
-                {variant}
-              </button>
+                onClick={() => setMainImage(productImage)}
+              />
             ))}
           </div>
-        </div>
-        <div className="space-y-2">
-          <h2 className="font-medium">Cor:</h2>
-          <div className="flex gap-2">
-            {variants.color.map((variant) => (
-              <button
-                key={variant}
-                onClick={() => setSelectedColor(variant)}
-                className={`px-4 py-2 border rounded ${
-                  selectedColor === variant
-                    ? 'bg-blue-600 text-white'
-                    : 'bg-white border-gray-300'
-                }`}
-              >
-                {variant}
-              </button>
-            ))}
+        </section>
+        <section className="space-y-6">
+          <div>
+            <h1 className="text-3xl font-semibold">Camiseta Estilosa</h1>
+            <p className="text-xl mt-1 text-gray-600 font-bold">R$ 79,90</p>
           </div>
-        </div>
-        <FreightForm
-          handleCepSearch={handleCepSearch}
-          cep={cep}
-          setCep={setCep}
-          address={address}
-          error={error}
-        />
-      </section>
-    </main>
+          <div className="space-y-2">
+            <h2 className="font-medium">Tamanho:</h2>
+            <div className="flex gap-2">
+              {variants.size.map((variant) => (
+                <button
+                  key={variant}
+                  onClick={() => setSelectedSize(variant)}
+                  className={`px-4 py-2 border rounded ${
+                    selectedSize === variant
+                      ? 'bg-blue-600 text-white'
+                      : 'bg-white border-gray-300'
+                  }`}
+                >
+                  {variant}
+                </button>
+              ))}
+            </div>
+          </div>
+          <div className="space-y-2">
+            <h2 className="font-medium">Cor:</h2>
+            <div className="flex gap-2">
+              {variants.color.map((variant) => (
+                <button
+                  key={variant}
+                  onClick={() => setSelectedColor(variant)}
+                  className={`px-4 py-2 border rounded ${
+                    selectedColor === variant
+                      ? 'bg-blue-600 text-white'
+                      : 'bg-white border-gray-300'
+                  }`}
+                >
+                  {variant}
+                </button>
+              ))}
+            </div>
+          </div>
+          <FreightForm
+            handleCepSearch={handleCepSearch}
+            cep={cep}
+            setCep={setCep}
+            address={address}
+            error={error}
+          />
+        </section>
+      </main>
+      <Footer />
+    </>
   )
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <div className="w-full h-12 border-t border-gray-200 flex items-center justify-center mt-8">
+      Interface made with 💕
+    </div>
+  )
+}
