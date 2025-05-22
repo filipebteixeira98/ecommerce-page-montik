4460f00620f89dc15976004002f8f5f74bffcec5
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2025-05-22 00:52:24 -0300

    chore(core): create hardcoded data

---
+++ src/utils/data.ts
@@ -0,0 +1,11 @@
+export const productImages = [
+  '/product1.jpg',
+  '/product2.jpg',
+  '/product3.jpg',
+  '/product4.jpg',
+]
+
+export const variants = {
+  size: ['P', 'M', 'G', 'GG'],
+  color: ['Preto', 'Branco', 'Azul', 'Amarelo'],
+}
