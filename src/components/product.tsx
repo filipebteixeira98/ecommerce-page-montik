a254d8c87a380a9e804ffa951940c35ee377064e
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2025-05-22 02:59:30 -0300

    chore(product): component texts

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -8,13 +8,13 @@ export function Product() {
     <div className="flex flex-col">
       <div className="flex justify-end items-center py-4">
         <a href="#share" className="text-sm text-blue-500 px-1">
-          Share
+          Compartilhar
         </a>
         <a
           href="#sell"
           className="text-sm text-blue-500 px-1 border-l border-gray-300 ml-2 pl-2"
         >
-          Sell an equal
+          Vender produto igual
         </a>
       </div>
       <div className="grid grid-cols-[65fr_35fr] bg-white shadow-md">
@@ -37,23 +37,25 @@ export function Product() {
 function WarrantySection() {
   return (
     <div className="border-t border-gray-300 p-12 flex flex-col">
-      <h4 className="text-lg mb-10">Warranty</h4>
+      <h4 className="text-lg mb-10">Garantia</h4>
       <div className="flex flex-col">
         <span>
           <p className="text-base text-black">
-            Security guarantee in your purchase
+            Garantia de segurança na sua compra
           </p>
           <p className="mt-1 text-sm text-gray-600 leading-5">
-            Receive product you are waiting for or refund your money
+            Receba o produto que você esperava ou devolva seu dinheiro
           </p>
         </span>
         <span className="mt-4">
-          <p className="text-base text-black">Seller guarantee</p>
-          <p className="mt-1 text-sm text-gray-600 leading-5">No warranty</p>
+          <p className="text-base text-black">Garantia do vendedor</p>
+          <p className="mt-1 text-sm text-gray-600 leading-5">
+            Sem garantia Saiba mais sobre a garantia
+          </p>
         </span>
       </div>
       <a href="#learn" className="mt-5 font-semibold text-sm text-blue-500">
-        Learn more about the warranty
+        Saiba mais sobre a garantia
       </a>
     </div>
   )
