9945e040cfb54e87ec6ad3fa1ec29d2da2c088c8
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2025-05-22 02:41:54 -0300

    chore(core): create product component

---
+++ src/components/product.tsx
@@ -0,0 +1,83 @@
+import Image from 'next/image'
+
+import { ProductAction } from '@/components/product-action'
+import { SellerInfo } from '@/components/seller-info'
+
+export function Product() {
+  return (
+    <div className="flex flex-col">
+      <div className="flex justify-end items-center py-4">
+        <a href="#share" className="text-sm text-blue-500 px-1">
+          Share
+        </a>
+        <a
+          href="#sell"
+          className="text-sm text-blue-500 px-1 border-l border-gray-300 ml-2 pl-2"
+        >
+          Sell an equal
+        </a>
+      </div>
+      <div className="grid grid-cols-[65fr_35fr] bg-white shadow-md">
+        <div className="border-r border-gray-300">
+          <div className="flex items-center justify-center h-[530px]">
+            <Image src={''} alt="T-shirt" className="h-[73%]" />
+          </div>
+          <Description />
+        </div>
+        <div>
+          <ProductAction />
+          <SellerInfo />
+          <WarrantySection />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function WarrantySection() {
+  return (
+    <div className="border-t border-gray-300 p-12 flex flex-col">
+      <h4 className="text-lg mb-10">Warranty</h4>
+      <div className="flex flex-col">
+        <span>
+          <p className="text-base text-black">
+            Security guarantee in your purchase
+          </p>
+          <p className="mt-1 text-sm text-gray-600 leading-5">
+            Receive product you are waiting for or refund your money
+          </p>
+        </span>
+        <span className="mt-4">
+          <p className="text-base text-black">Seller guarantee</p>
+          <p className="mt-1 text-sm text-gray-600 leading-5">No warranty</p>
+        </span>
+      </div>
+      <a href="#learn" className="mt-5 font-semibold text-sm text-blue-500">
+        Learn more about the warranty
+      </a>
+    </div>
+  )
+}
+
+function Description() {
+  return (
+    <div className="border-t border-gray-300 px-8 py-11">
+      <h2 className="text-2xl mb-8">Description</h2>
+      <p className="text-lg text-gray-600 leading-7">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tristique,
+        leo quis pharetra mollis...
+        <br />
+        <br />
+        Elementum faucibus: <br />
+        - 1x Lorem ipsum <br />
+        - 2x Dolor sit amet <br />
+        - 5x Pharetra mollis <br />
+        - 3x Nam nisl <br />
+        - 4x Curabitur non erat <br />
+        <br />
+        Praesent magna magna, fermentum sit amet gravida eget, sodales sit amet
+        justo. Mauris ultrices placerat euismod...
+      </p>
+    </div>
+  )
+}
