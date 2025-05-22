317dada96778d77d85455662080a25686189d9e3
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2025-05-22 03:57:32 -0300

    chore(core): configure jest and related libraries for test environment

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+}
+
+export default config
