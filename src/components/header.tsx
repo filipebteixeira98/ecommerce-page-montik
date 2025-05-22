193ab2d384efadbcf31a716b96c4a6222996e1db
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2025-05-22 02:18:47 -0300

    chore(header): add fixed property

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import { ShoppingCart } from 'lucide-react'
 
 export function Header() {
   return (
-    <div className="w-full h-12 bg-yellow-300 border-b border-gray-100 flex items-center justify-center">
+    <div className="w-full h-12 fixed bg-yellow-300 border-b border-gray-100 flex items-center justify-center">
       <div className="flex items-center gap-2">
         <ShoppingCart className="size-5" />
         <span className="text-base uppercase">Free Market</span>
